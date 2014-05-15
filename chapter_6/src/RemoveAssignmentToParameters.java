9e6bb9861d212e6e7c2372cf23bcf3a09bc62f04
tezuka_y <tezuka_y@mti.co.jp>
2014-05-15 18:47:16 +0900

    Chapter6 : (131) Remove Assignment to Parameters

---
+++ chapter_6/src/RemoveAssignmentToParameters.java
@@ -0,0 +1,17 @@
+public class RemoveAssignmentToParameters {
+
+    int discount(final int inputVal, final int quantity, final int yearToDate) {
+        int result = inputVal;
+        if (inputVal > 50) {
+            result -= 2;
+        }
+        if (quantity > 100) {
+            result -= 1;
+        }
+        if (yearToDate > 10000) {
+            result -= 4;
+        }
+        return result;
+    }
+
+}
