830a3ca0724234fc62f27d70034c8ddb9fa76b00
YousukeTezuka <YousukeTezuka@gmail.com>
2014-05-16 11:45:18 +0900

    Chapter7 : (149) Extract Class

--- chapter_7/src/ExtractClass.java
+++ chapter_7/src/ExtractClass.java
@@ -1,4 +1,51 @@
-
 public class ExtractClass {
+
+	private class Person {
+
+		private String _name;
+
+		private final TelephoneNumber _officeTelephone = new TelephoneNumber();
+
+		public String getName() {
+			return _name;
+		}
+
+		public String getTelephoneNumber() {
+			return _officeTelephone.getTelephoneNumber();
+		}
+
+		TelephoneNumber getOfficeTelephone() {
+			return _officeTelephone;
+		}
+
+		private class TelephoneNumber {
+
+			private String _areaCode;
+
+			private String _number;
+
+			public String getTelephoneNumber() {
+				return "(" + _areaCode + ")" + _number;
+			}
+
+			String getAreaCode() {
+				return _areaCode;
+			}
+
+			void setAreaCode(String arg) {
+				_areaCode = arg;
+			}
+
+			String getNumber() {
+				return _number;
+			}
+
+			void setNumber(String number) {
+				_number = number;
+			}
+
+		}
+
+	}
 
 }
