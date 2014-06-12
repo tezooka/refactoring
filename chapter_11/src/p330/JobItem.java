8e00647a5fc60c3646384c2260d184b262d514b3
tezuka_y <tezuka_y@mti.co.jp>
2014-06-12 18:09:59 +0900

    Chapter11 : (330) Extract Subclass

--- chapter_11/src/p330/JobItem.java
+++ chapter_11/src/p330/JobItem.java
@@ -1,9 +1,27 @@
 package p330;
 
-public class JobItem {
+public abstract class JobItem {
 
-    public JobItem() {
+    private final int _quantity;
 
+    protected Employee _employee;
+
+    public JobItem(int quantity) {
+        _quantity = quantity;
+    }
+
+    public int getTotalPrice() {
+        return getUnitPrice() + _quantity;
+    }
+
+    abstract int getUnitPrice();
+
+    public int getQuantity() {
+        return _quantity;
+    }
+
+    public boolean isLabor() {
+        return false;
     }
 
 }
